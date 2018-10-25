22ef3663af701c6b1f60d979b9d54ce0fb0ae93c
taylorchu <tailinchu@gmail.com>
2018-10-25 10:11:50 -0700

    benchmark unique enqueuer

--- middleware/unique/enqueuer_test.go
+++ middleware/unique/enqueuer_test.go
@@ -29,25 +29,64 @@ func TestEnqueuer(t *testing.T) {
 		},
 	})
 
-	opt := &work.EnqueueOptions{
-		Namespace: "n1",
-		QueueID:   "q1",
-	}
 	var called int
 	h := enq(func(*work.Job, *work.EnqueueOptions) error {
 		called++
 		return nil
 	})
 	for i := 0; i < 3; i++ {
-		err := h(work.NewJob(), opt)
+		job := work.NewJob()
+		err := h(job, &work.EnqueueOptions{
+			Namespace: "n1",
+			QueueID:   "q1",
+			At:        job.CreatedAt,
+		})
 		require.NoError(t, err)
 	}
 	require.Equal(t, 1, called)
 
 	for i := 0; i < 3; i++ {
 		require.NoError(t, client.Del("n1:unique:q1:e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855").Err())
-		err := h(work.NewJob(), opt)
+		job := work.NewJob()
+		err := h(job, &work.EnqueueOptions{
+			Namespace: "n1",
+			QueueID:   "q1",
+			At:        job.CreatedAt,
+		})
 		require.NoError(t, err)
 		require.Equal(t, i+2, called)
 	}
+}
+
+func BenchmarkEnqueuer(b *testing.B) {
+	b.StopTimer()
+
+	client := newRedisClient()
+	defer client.Close()
+	require.NoError(b, client.FlushAll().Err())
+
+	enq := Enqueuer(&EnqueuerOptions{
+		Client: client,
+		UniqueFunc: func(job *work.Job, _ *work.EnqueueOptions) ([]byte, time.Duration) {
+			return []byte(job.ID), time.Hour
+		},
+	})
+
+	var called int
+	h := enq(func(*work.Job, *work.EnqueueOptions) error {
+		called++
+		return nil
+	})
+
+	b.StartTimer()
+	for n := 0; n < b.N; n++ {
+		job := work.NewJob()
+		h(job, &work.EnqueueOptions{
+			Namespace: "n1",
+			QueueID:   "q1",
+			At:        job.CreatedAt,
+		})
+	}
+	b.StopTimer()
+	require.Equal(b, b.N, called)
 }
