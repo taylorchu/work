449064d5c582293d123ac22d969d536c0ee70cb5
taylorchu <tailinchu@gmail.com>
2020-04-19 11:28:23 -0700

    TWEAK: add catch panic middleware

---
+++ middleware/recovery/catch_panic_test.go
@@ -0,0 +1,22 @@
+package recovery
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/taylorchu/work"
+)
+
+func TestCatchPanic(t *testing.T) {
+	job := work.NewJob()
+	opt := &work.DequeueOptions{
+		Namespace: "n1",
+		QueueID:   "q1",
+	}
+	h := CatchPanic(func(*work.Job, *work.DequeueOptions) error {
+		panic("fatal error")
+	})
+
+	err := h(job, opt)
+	require.Error(t, err)
+}
