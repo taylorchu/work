19402214f44d8f0643af1f379f7ec78d16426d34
taylorchu <tailinchu@gmail.com>
2020-04-19 11:33:47 -0700

    TWEAK: add catch panic middleware (#26)

---
+++ middleware/recovery/catch_panic.go
@@ -0,0 +1,25 @@
+package recovery
+
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/taylorchu/work"
+)
+
+// CatchPanic catches a panic from handler.
+// It also adds runtime stack for debugging.
+func CatchPanic(f work.HandleFunc) work.HandleFunc {
+	return func(job *work.Job, opt *work.DequeueOptions) (err error) {
+		defer func() {
+			if r := recover(); r != nil {
+				const size = 4096
+				stack := make([]byte, size)
+				stack = stack[:runtime.Stack(stack, false)]
+
+				err = fmt.Errorf("panic: %v\n\n%s", r, stack)
+			}
+		}()
+		return f(job, opt)
+	}
+}
