c5e1f71b4fb75382df41131468f816cade173ee4
taylorchu <tailinchu@gmail.com>
2020-11-20 08:58:31 -0800

    TWEAK: handle pre expired jobs in discard.After

--- middleware/discard/after.go
+++ middleware/discard/after.go
@@ -10,14 +10,14 @@ import (
 func After(d time.Duration) work.HandleMiddleware {
 	return func(f work.HandleFunc) work.HandleFunc {
 		return func(job *work.Job, opt *work.DequeueOptions) error {
-			err := f(job, opt)
-			if err != nil {
-				if time.Now().Sub(job.CreatedAt) > d {
-					return work.ErrUnrecoverable
-				}
-				return err
+			if time.Since(job.CreatedAt) > d {
+				return work.ErrUnrecoverable
 			}
-			return nil
+			err := f(job, opt)
+			if time.Since(job.CreatedAt) > d {
+				return work.ErrUnrecoverable
+			}
+			return err
 		}
 	}
 }
