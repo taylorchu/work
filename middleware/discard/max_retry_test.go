071759c532505213e48b5b1dc2994eaafc48990d
taylorchu <tailinchu@gmail.com>
2020-12-05 19:05:00 -0800

    TWEAK: add discard max retry middleware

---
+++ middleware/discard/max_retry_test.go
@@ -0,0 +1,29 @@
+package discard
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/taylorchu/work"
+)
+
+func TestMaxRetry(t *testing.T) {
+	job := work.NewJob()
+	opt := &work.DequeueOptions{
+		Namespace: "n1",
+		QueueID:   "q1",
+	}
+	d := MaxRetry(1)
+	h := d(func(*work.Job, *work.DequeueOptions) error {
+		return errors.New("no reason")
+	})
+
+	err := h(job, opt)
+	require.Error(t, err)
+	require.NotEqual(t, work.ErrUnrecoverable, err)
+
+	job.Retries = 1
+	err = h(job, opt)
+	require.Equal(t, work.ErrUnrecoverable, err)
+}
