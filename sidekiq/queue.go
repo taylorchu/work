c9054bd916fe98b619ec8c4221dbdc20f58ea565
taylorchu <tailinchu@gmail.com>
2021-03-18 22:09:36 -0700

    TWEAK: reduce sidekiq scheduled job delay

--- sidekiq/queue.go
+++ sidekiq/queue.go
@@ -12,11 +12,12 @@ import (
 )
 
 type sidekiqQueue struct {
-	redisQueue     work.Queue
-	client         redis.UniversalClient
-	enqueueScript  *redis.Script
-	dequeueScript  *redis.Script
-	scheduleScript *redis.Script
+	redisQueue      work.Queue
+	client          redis.UniversalClient
+	enqueueScript   *redis.Script
+	enqueueInScript *redis.Script
+	dequeueScript   *redis.Script
+	scheduleScript  *redis.Script
 }
 
 // See https://github.com/mperham/sidekiq/wiki/Job-Format.
@@ -75,6 +76,24 @@ func (j *sidekiqJob) Validate() error {
 func NewQueue(client redis.UniversalClient) work.Queue {
 	// https://github.com/mperham/sidekiq/blob/e3839682a3d219b8a3708feab607c74241bc06b8/lib/sidekiq/client.rb#L190
 	enqueueScript := redis.NewScript(`
+	local ns = ARGV[1]
+	local sidekiq_queue = ARGV[2]
+
+	local lpush_args = {}
+	local queues_key = table.concat({ns, "queues"}, ":")
+	local queue_key = table.concat({ns, "queue", sidekiq_queue}, ":")
+
+	for i = 3,table.getn(ARGV) do
+		local jobm = ARGV[i]
+
+		-- enqueue
+		table.insert(lpush_args, jobm)
+	end
+	redis.call("sadd", queues_key, sidekiq_queue)
+	return redis.call("lpush", queue_key, unpack(lpush_args))
+	`)
+
+	enqueueInScript := redis.NewScript(`
 	local ns = ARGV[1]
 	local schedule_key = table.concat({ns, "schedule"}, ":")
 
@@ -136,11 +155,12 @@ func NewQueue(client redis.UniversalClient) work.Queue {
 	`)
 
 	return &sidekiqQueue{
-		redisQueue:     work.NewRedisQueue(client),
-		client:         client,
-		enqueueScript:  enqueueScript,
-		dequeueScript:  dequeueScript,
-		scheduleScript: scheduleScript,
+		redisQueue:      work.NewRedisQueue(client),
+		client:          client,
+		enqueueScript:   enqueueScript,
+		enqueueInScript: enqueueInScript,
+		dequeueScript:   dequeueScript,
+		scheduleScript:  scheduleScript,
 	}
 }
 
@@ -209,6 +229,56 @@ func (q *sidekiqQueue) BulkEnqueue(jobs []*work.Job, opt *work.EnqueueOptions) e
 	if err != nil {
 		return err
 	}
+
+	now := time.Now()
+	readyJobs := make([]*work.Job, 0, len(jobs))
+	scheduledJobs := make([]*work.Job, 0, len(jobs))
+	for _, job := range jobs {
+		if job.EnqueuedAt.After(now) {
+			scheduledJobs = append(scheduledJobs, job)
+		} else {
+			readyJobs = append(readyJobs, job)
+		}
+	}
+
+	err = q.bulkEnqueue(readyJobs, opt)
+	if err != nil {
+		return err
+	}
+	err = q.bulkEnqueueIn(scheduledJobs, opt)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (q *sidekiqQueue) bulkEnqueue(jobs []*work.Job, opt *work.EnqueueOptions) error {
+	if len(jobs) == 0 {
+		return nil
+	}
+	sqQueue, sqClass := parseQueueID(opt.QueueID)
+	args := make([]interface{}, 2+len(jobs))
+	args[0] = opt.Namespace
+	args[1] = sqQueue
+	for i, job := range jobs {
+		sqJob, err := newSidekiqJob(job, sqQueue, sqClass)
+		if err != nil {
+			return err
+		}
+		err = sqJob.Validate()
+		if err != nil {
+			return err
+		}
+		jobm, err := json.Marshal(sqJob)
+		if err != nil {
+			return err
+		}
+		args[2+i] = jobm
+	}
+	return q.enqueueScript.Run(q.client, nil, args...).Err()
+}
+
+func (q *sidekiqQueue) bulkEnqueueIn(jobs []*work.Job, opt *work.EnqueueOptions) error {
 	if len(jobs) == 0 {
 		return nil
 	}
@@ -231,7 +301,7 @@ func (q *sidekiqQueue) BulkEnqueue(jobs []*work.Job, opt *work.EnqueueOptions) e
 		args[1+2*i] = job.EnqueuedAt.Unix()
 		args[1+2*i+1] = jobm
 	}
-	return q.enqueueScript.Run(q.client, nil, args...).Err()
+	return q.enqueueInScript.Run(q.client, nil, args...).Err()
 }
 
 func (q *sidekiqQueue) Dequeue(opt *work.DequeueOptions) (*work.Job, error) {
