8b0a813a469ba3f15cf2d7c44ac6768531bdf711
taylorchu <tailinchu@gmail.com>
2021-06-01 20:37:32 -0700

    fix example os.kill cannot be trapped

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -68,6 +68,6 @@ func main() {
 	defer w.Stop()
 
 	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, os.Kill, syscall.SIGTERM)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	<-c
 }
