bd4ea67bf5850436d1ffa8e6fd21259522d47263
taylorchu <tailinchu@gmail.com>
2021-08-31 09:05:59 -0700

    FEATURE: add heartbeat middleware

---
+++ middleware/heartbeat/heartbeater_test.go
@@ -0,0 +1,68 @@
+package heartbeat
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/stretchr/testify/require"
+	"github.com/taylorchu/work"
+	"github.com/taylorchu/work/redistest"
+)
+
+func TestHeartbeater(t *testing.T) {
+	client := redistest.NewClient()
+	defer client.Close()
+	require.NoError(t, redistest.Reset(client))
+
+	job := work.NewJob()
+	opt := &work.DequeueOptions{
+		Namespace:    "{ns1}",
+		QueueID:      "q1",
+		At:           time.Now(),
+		InvisibleSec: 60,
+	}
+
+	hb := Heartbeater(&HeartbeaterOptions{
+		Queue:        work.NewRedisQueue(client),
+		InvisibleSec: 30,
+		IntervalSec:  1,
+	})
+
+	h := hb(func(*work.Job, *work.DequeueOptions) error {
+		return nil
+	})
+
+	err := h(job, opt)
+	require.NoError(t, err)
+	require.Equal(t, job.CreatedAt, job.UpdatedAt)
+	require.Equal(t, job.CreatedAt, job.EnqueuedAt)
+
+	z, err := client.ZRangeByScoreWithScores(
+		context.Background(),
+		"{ns1}:queue:q1",
+		&redis.ZRangeBy{
+			Min: "-inf",
+			Max: "+inf",
+		}).Result()
+	require.NoError(t, err)
+	require.Len(t, z, 1)
+	require.EqualValues(t, job.EnqueuedAt.Unix()+30, z[0].Score)
+
+	err = h(job, opt)
+	require.NoError(t, err)
+	require.Equal(t, job.CreatedAt, job.UpdatedAt)
+	require.Equal(t, job.CreatedAt, job.EnqueuedAt)
+
+	z, err = client.ZRangeByScoreWithScores(
+		context.Background(),
+		"{ns1}:queue:q1",
+		&redis.ZRangeBy{
+			Min: "-inf",
+			Max: "+inf",
+		}).Result()
+	require.NoError(t, err)
+	require.Len(t, z, 1)
+	require.EqualValues(t, job.EnqueuedAt.Unix()+30, z[0].Score)
+}
