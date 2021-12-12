e245505ef9965ac6e7fe453398bac8ebcb925e34
taylorchu <tailinchu@gmail.com>
2021-12-11 23:50:17 -0800

    wip

--- README.md
+++ README.md
@@ -10,35 +10,40 @@ Please see `cmd/` for enqueuer and worker demo.
 ## Improvements
 
 - [x] queue backend abstraction
-    - redis is still the default, but the new design allows custom queue implementation.
+  - redis is still the default, but the new design allows custom queue implementation.
 - [x] simplify the keyspace design of redis queue backend
-    - The new design uses 1 redis hash per job, and 1 redis sorted set for queue.
-    - [Interesting read](https://kirshatrov.com/2018/07/20/redis-job-queue/)
+  - The new design uses 1 redis hash per job, and 1 redis sorted set for queue.
+  - [Interesting read](https://kirshatrov.com/2018/07/20/redis-job-queue/)
 - [x] modular
-    - The core only catches panics, retries on failure, and waits if a queue is empty.
-    - All other [functionalities](https://kirshatrov.com/2019/01/03/state-of-background-jobs/)
-      are either removed or moved to separate middlewares.
+  - The core only catches panics, retries on failure, and waits if a queue is empty.
+  - All other [functionalities](https://kirshatrov.com/2019/01/03/state-of-background-jobs/)
+    are either removed or moved to separate middlewares.
 - [x] support binary payload/args with message pack.
 - [x] replace built-in UI with prometheus metrics (use grafana if you want dashboard).
 - [x] additional optimizations (alloc + bulk queue ops)
-    ```go
-    BenchmarkWorkerRunJob/work_v1_1-8         	    3000	    515957 ns/op
-    BenchmarkWorkerRunJob/work_v2_1-8         	    5000	    284516 ns/op
-    BenchmarkWorkerRunJob/work_v1_10-8        	    1000	   2136546 ns/op
-    BenchmarkWorkerRunJob/work_v2_10-8        	    5000	    367997 ns/op
-    BenchmarkWorkerRunJob/work_v1_100-8       	     100	  18234023 ns/op
-    BenchmarkWorkerRunJob/work_v2_100-8       	    1000	   1759186 ns/op
-    BenchmarkWorkerRunJob/work_v1_1000-8      	      10	 162110100 ns/op
-    BenchmarkWorkerRunJob/work_v2_1000-8      	     100	  12646080 ns/op
-    BenchmarkWorkerRunJob/work_v1_10000-8     	       1	1691287122 ns/op
-    BenchmarkWorkerRunJob/work_v2_10000-8     	      10	 144923087 ns/op
-    BenchmarkWorkerRunJob/work_v1_100000-8    	       1	17515722574 ns/op
-    BenchmarkWorkerRunJob/work_v2_100000-8    	       1	1502468637 ns/op
-    PASS
-    ok  	github.com/taylorchu/work	87.901s
-    ```
-- [ ] http enqueuer
-
+  ```go
+  BenchmarkWorkerRunJob/work_v1_1-8         	    3000	    515957 ns/op
+  BenchmarkWorkerRunJob/work_v2_1-8         	    5000	    284516 ns/op
+  BenchmarkWorkerRunJob/work_v1_10-8        	    1000	   2136546 ns/op
+  BenchmarkWorkerRunJob/work_v2_10-8        	    5000	    367997 ns/op
+  BenchmarkWorkerRunJob/work_v1_100-8       	     100	  18234023 ns/op
+  BenchmarkWorkerRunJob/work_v2_100-8       	    1000	   1759186 ns/op
+  BenchmarkWorkerRunJob/work_v1_1000-8      	      10	 162110100 ns/op
+  BenchmarkWorkerRunJob/work_v2_1000-8      	     100	  12646080 ns/op
+  BenchmarkWorkerRunJob/work_v1_10000-8     	       1	1691287122 ns/op
+  BenchmarkWorkerRunJob/work_v2_10000-8     	      10	 144923087 ns/op
+  BenchmarkWorkerRunJob/work_v1_100000-8    	       1	17515722574 ns/op
+  BenchmarkWorkerRunJob/work_v2_100000-8    	       1	1502468637 ns/op
+  PASS
+  ok  	github.com/taylorchu/work	87.901s
+  ```
+- [x] http server
+  - [x] delete job
+  - [x] create job
+  - [x] get job status
+  - [x] get queue metrics (kubernetes autoscaler integration with [keda metrics api scaler](https://keda.sh/docs/2.5/scalers/metrics-api/))
+  - [ ] OpenAPI spec
 
 ## License
+
 [![FOSSA Status](https://app.fossa.com/api/projects/custom%2B4257%2Fgit%40github.com%3Ataylorchu%2Fwork.git.svg?type=large)](https://app.fossa.com/projects/custom%2B4257%2Fgit%40github.com%3Ataylorchu%2Fwork.git?ref=badge_large)
---
+++ http/server.go
@@ -0,0 +1,235 @@
+package http
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/taylorchu/work"
+)
+
+type ServerOptions struct {
+	Queue work.Queue
+}
+
+func NewServer(opts *ServerOptions) http.Handler {
+	m := http.NewServeMux()
+	m.HandleFunc("/jobs", func(rw http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "DELETE":
+			queue, ok := opts.Queue.(work.BulkJobFinder)
+			if !ok {
+				rw.WriteHeader(http.StatusNotFound)
+				return
+			}
+			namespace := r.URL.Query().Get("namespace")
+			jobID := r.URL.Query().Get("job_id")
+			queueID := r.URL.Query().Get("queue_id")
+			job, err := func() (*work.Job, error) {
+				jobs, err := queue.BulkFind([]string{jobID}, &work.FindOptions{
+					Namespace: namespace,
+				})
+				if err != nil {
+					return nil, err
+				}
+				if len(jobs) == 1 && jobs[0] != nil {
+					err := opts.Queue.Ack(jobs[0], &work.AckOptions{
+						Namespace: namespace,
+						QueueID:   queueID,
+					})
+					if err != nil {
+						return nil, err
+					}
+					return jobs[0], nil
+				}
+				return &work.Job{
+					ID: jobID,
+				}, nil
+			}()
+			if err != nil {
+				rw.WriteHeader(http.StatusInternalServerError)
+				json.NewEncoder(rw).Encode(struct {
+					Error string `json:"error"`
+				}{
+					Error: err.Error(),
+				})
+				return
+			}
+			json.NewEncoder(rw).Encode(struct {
+				Namespace string    `json:"namespace"`
+				QueueID   string    `json:"queue_id"`
+				Job       *work.Job `json:"job"`
+			}{
+				Namespace: namespace,
+				QueueID:   queueID,
+				Job:       job,
+			})
+		case "GET":
+			queue, ok := opts.Queue.(work.BulkJobFinder)
+			if !ok {
+				rw.WriteHeader(http.StatusNotFound)
+				return
+			}
+			namespace := r.URL.Query().Get("namespace")
+			jobID := r.URL.Query().Get("job_id")
+			job, err := func() (*work.Job, error) {
+				jobs, err := queue.BulkFind([]string{jobID}, &work.FindOptions{
+					Namespace: namespace,
+				})
+				if err != nil {
+					return nil, err
+				}
+				if len(jobs) == 1 && jobs[0] != nil {
+					return jobs[0], nil
+				}
+				return &work.Job{
+					ID: jobID,
+				}, nil
+			}()
+			if err != nil {
+				rw.WriteHeader(http.StatusInternalServerError)
+				json.NewEncoder(rw).Encode(struct {
+					Error string `json:"error"`
+				}{
+					Error: err.Error(),
+				})
+				return
+			}
+			json.NewEncoder(rw).Encode(struct {
+				Namespace string    `json:"namespace"`
+				Status    string    `json:"status"`
+				Job       *work.Job `json:"job"`
+			}{
+				Namespace: namespace,
+				Status:    jobStatus(job),
+				Job:       job,
+			})
+		case "POST":
+			namespace := r.URL.Query().Get("namespace")
+			queueID := r.URL.Query().Get("queue_id")
+
+			job, err := func() (*work.Job, error) {
+				var enqueueRequest struct {
+					ID      string          `json:"id"`
+					Payload json.RawMessage `json:"payload"`
+					Delay   Duration        `json:"delay"`
+				}
+				err := json.NewDecoder(r.Body).Decode(&enqueueRequest)
+				if err != nil {
+					return nil, err
+				}
+				job := work.NewJob().Delay(time.Duration(enqueueRequest.Delay))
+				if enqueueRequest.ID != "" {
+					job.ID = enqueueRequest.ID
+				}
+				job.Payload = enqueueRequest.Payload
+				err = opts.Queue.Enqueue(job, &work.EnqueueOptions{
+					Namespace: namespace,
+					QueueID:   queueID,
+				})
+				if err != nil {
+					return nil, err
+				}
+				return job, nil
+			}()
+			if err != nil {
+				rw.WriteHeader(http.StatusInternalServerError)
+				json.NewEncoder(rw).Encode(struct {
+					Error string `json:"error"`
+				}{
+					Error: err.Error(),
+				})
+				return
+			}
+			json.NewEncoder(rw).Encode(struct {
+				Namespace string    `json:"namespace"`
+				QueueID   string    `json:"queue_id"`
+				Job       *work.Job `json:"job"`
+			}{
+				Namespace: namespace,
+				QueueID:   queueID,
+				Job:       job,
+			})
+		default:
+			rw.WriteHeader(http.StatusNotFound)
+		}
+	})
+	m.HandleFunc("/metrics", func(rw http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "GET":
+			queue, ok := opts.Queue.(work.MetricsExporter)
+			if !ok {
+				rw.WriteHeader(http.StatusNotFound)
+				return
+			}
+			namespace := r.URL.Query().Get("namespace")
+			queueID := r.URL.Query().Get("queue_id")
+
+			metrics, err := queue.GetQueueMetrics(&work.QueueMetricsOptions{
+				Namespace: namespace,
+				QueueID:   queueID,
+				At:        time.Now(),
+			})
+			if err != nil {
+				rw.WriteHeader(http.StatusInternalServerError)
+				json.NewEncoder(rw).Encode(struct {
+					Error string `json:"error"`
+				}{
+					Error: err.Error(),
+				})
+				return
+			}
+			json.NewEncoder(rw).Encode(struct {
+				Namespace      string `json:"namespace"`
+				QueueID        string `json:"queue_id"`
+				ReadyTotal     int64  `json:"ready_total"`
+				ScheduledTotal int64  `json:"scheduled_total"`
+				Total          int64  `json:"total"`
+			}{
+				Namespace:      metrics.Namespace,
+				QueueID:        metrics.QueueID,
+				ReadyTotal:     metrics.ReadyTotal,
+				ScheduledTotal: metrics.ScheduledTotal,
+				Total:          metrics.ReadyTotal + metrics.ScheduledTotal,
+			})
+		default:
+			rw.WriteHeader(http.StatusNotFound)
+		}
+	})
+	return m
+}
+
+func jobStatus(job *work.Job) string {
+	if job.EnqueuedAt.IsZero() {
+		return "completed"
+	}
+	if job.EnqueuedAt.After(time.Now()) {
+		return "scheduled"
+	}
+	return "ready"
+}
+
+type Duration time.Duration
+
+func (d Duration) MarshalJSON() ([]byte, error) {
+	return json.Marshal(time.Duration(d).String())
+}
+
+func (d *Duration) UnmarshalJSON(b []byte) error {
+	var v interface{}
+	if err := json.Unmarshal(b, &v); err != nil {
+		return err
+	}
+	switch value := v.(type) {
+	case string:
+		tmp, err := time.ParseDuration(value)
+		if err != nil {
+			return err
+		}
+		*d = Duration(tmp)
+		return nil
+	default:
+		return fmt.Errorf("invalid duration: %v", v)
+	}
+}
