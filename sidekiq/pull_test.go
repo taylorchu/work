28607b81544bf207e4198d46ead1184611c6d58e
taylorchu <tailinchu@gmail.com>
2023-04-14 18:48:40 -0700

    ok

--- sidekiq/pull_test.go
+++ sidekiq/pull_test.go
@@ -154,12 +154,9 @@ func TestPullDequeueStartRecoveredNotExpired(t *testing.T) {
 	).Err()
 	require.NoError(t, err)
 
-	err = client.LPush(context.Background(), "{sidekiq}:queue:default", `{"class":"TestWorker","args":[],"retry":3,"queue":"default","backtrace":true,"jid":"83b27ea26dd65821239ca6aa","created_at":1567788641.0875323,"enqueued_at":1567788642.0879307,"retry_count":2,"error_message":"error: test","error_class":"StandardError","failed_at":1567791043,"retried_at":1567791046}"`).Err()
-	require.NoError(t, err)
-
 	count, err = client.Exists(context.Background(), "{sidekiq}:queue:default").Result()
 	require.NoError(t, err)
-	require.Equal(t, int64(1), count)
+	require.Equal(t, int64(0), count)
 
 	count, err = client.Exists(context.Background(), "{sidekiq}:sidekiq-queue-pull:default:123").Result()
 	require.NoError(t, err)
@@ -176,6 +173,13 @@ func TestPullDequeueStartRecoveredNotExpired(t *testing.T) {
 	require.Len(t, z, 1)
 	require.Equal(t, "123", z[0].Member)
 	require.EqualValues(t, now.Unix()+10, z[0].Score)
+
+	err = client.LPush(context.Background(), "{sidekiq}:queue:default", `{"class":"TestWorker","args":[],"retry":3,"queue":"default","backtrace":true,"jid":"83b27ea26dd65821239ca6aa","created_at":1567788641.0875323,"enqueued_at":1567788642.0879307,"retry_count":2,"error_message":"error: test","error_class":"StandardError","failed_at":1567791043,"retried_at":1567791046}"`).Err()
+	require.NoError(t, err)
+
+	count, err = client.Exists(context.Background(), "{sidekiq}:queue:default").Result()
+	require.NoError(t, err)
+	require.Equal(t, int64(1), count)
 
 	err = q.(*sidekiqQueue).dequeueStartScript.Run(context.Background(), client, nil,
 		"{sidekiq}",
@@ -238,12 +242,9 @@ func TestPullDequeueStartRecoveredExpired(t *testing.T) {
 	).Err()
 	require.NoError(t, err)
 
-	err = client.LPush(context.Background(), "{sidekiq}:queue:default", `{"class":"TestWorker","args":[],"retry":3,"queue":"default","backtrace":true,"jid":"83b27ea26dd65821239ca6aa","created_at":1567788641.0875323,"enqueued_at":1567788642.0879307,"retry_count":2,"error_message":"error: test","error_class":"StandardError","failed_at":1567791043,"retried_at":1567791046}"`).Err()
-	require.NoError(t, err)
-
 	count, err = client.Exists(context.Background(), "{sidekiq}:queue:default").Result()
 	require.NoError(t, err)
-	require.Equal(t, int64(1), count)
+	require.Equal(t, int64(0), count)
 
 	count, err = client.Exists(context.Background(), "{sidekiq}:sidekiq-queue-pull:default:123").Result()
 	require.NoError(t, err)
@@ -260,6 +261,13 @@ func TestPullDequeueStartRecoveredExpired(t *testing.T) {
 	require.Len(t, z, 1)
 	require.Equal(t, "123", z[0].Member)
 	require.EqualValues(t, now.Unix()+10, z[0].Score)
+
+	err = client.LPush(context.Background(), "{sidekiq}:queue:default", `{"class":"TestWorker","args":[],"retry":3,"queue":"default","backtrace":true,"jid":"83b27ea26dd65821239ca6aa","created_at":1567788641.0875323,"enqueued_at":1567788642.0879307,"retry_count":2,"error_message":"error: test","error_class":"StandardError","failed_at":1567791043,"retried_at":1567791046}"`).Err()
+	require.NoError(t, err)
+
+	count, err = client.Exists(context.Background(), "{sidekiq}:queue:default").Result()
+	require.NoError(t, err)
+	require.Equal(t, int64(1), count)
 
 	err = q.(*sidekiqQueue).dequeueStartScript.Run(context.Background(), client, nil,
 		"{sidekiq}",
@@ -342,7 +350,7 @@ func TestPullDequeueStop(t *testing.T) {
 	require.Len(t, z, 0)
 }
 
-func TestPullDequeueHeartbeatNormal(t *testing.T) {
+func TestPullDequeueHeartbeat(t *testing.T) {
 	client := redistest.NewClient()
 	defer client.Close()
 	require.NoError(t, redistest.Reset(client))
